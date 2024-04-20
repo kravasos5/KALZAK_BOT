e827b92fdb5b1def892750b4b1c33520c266ca39
Vladyslav Kravchenko <kravasos18@gmail.com>
2024-04-21 00:48:08 +0500

    creating keyboard

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,4 +1,7 @@
 import logging
+import sys
+from enum import Enum
+from pathlib import Path
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import (
@@ -10,68 +13,102 @@ from telegram.ext import (
 )
 from telegram.ext.filters import ALL
 
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+
+from src.config import settings
+
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-# Stages
-START_ROUTES, END_ROUTES, MAIN_MENU = range(3)
+
+# Стадии диалога
+class StagesEnum(Enum):
+    """Перечисление стадий диалога"""
+    START_CONVERSATION: int = 1
+    CHOOSE_STICKER_TYPE: int = 2
+    CHECK_SUBSCRIPTION: int = 3
+    AFTER_SEND_PHOTO: int = 4
+
+
 # Callback data
-DEFAULT_STICKERS, CUSTOM_STICKERS, AFTER_SEND_PHOTO, CHECKED_SUBSCRIBE = range(4)
+class CallbackEnum(Enum):
+    """Перечисление кодов ответа"""
+    DEFAULT_STICKERS: int = 1
+    CUSTOM_STICKERS: int = 2
+    START_CHECK_SUBSCRIPTION: int = 3
+    CHECK_SUBSCRIPTION: int = 4
+    SEND_PHOTO: int = 5
+    START_GENERATE: int = 6
+    END: int = 7
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
-    # Get user that sent /start and log his name
+    """Функция запуска диалога с ботом"""
+    # Получаю юзера
     user = update.message.from_user
+    # Добавляю лог
     logger.info("User %s started the conversation.", user.first_name)
-    # Build InlineKeyboard where each button has a displayed text
-    # and a string as callback_data
-    # The keyboard is a list of button rows, where each row is in turn
-    # a list (hence `[[...]]`).
+    # Создаю клавиатуру
     keyboard = [
         [
-            InlineKeyboardButton("Дефолтный стикерпак", callback_data=str(DEFAULT_STICKERS)),
-            InlineKeyboardButton("Кастомный стикерпак", callback_data=str(CUSTOM_STICKERS)),
+            InlineKeyboardButton("Сгенерировать стикер пак",
+                                 callback_data=str(CallbackEnum.START_CHECK_SUBSCRIPTION.value)),
+            InlineKeyboardButton("Завершить работу", callback_data=str(CallbackEnum.END.value)),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Send message with text and appended InlineKeyboard
-    await update.message.reply_text("Выберите, что хотите сгенерировать", reply_markup=reply_markup)
-    # Tell ConversationHandler that we're in state `FIRST` now
-    return START_ROUTES
+    # Отправляю клавиатуру
+    await update.message.reply_text("Что будем делать ?", reply_markup=reply_markup)
+    return StagesEnum.START_CONVERSATION.value
 
 
-async def start_over(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
-    # Get CallbackQuery from Update
+async def start_check_subscription(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Проверка подписки на тг канал"""
+    # Получаю юзера
+    user = update.callback_query.from_user
     query = update.callback_query
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    await query.answer()
+    # Добавляю лог
+    logger.info("User %s started the subscribe checking process.", user['first_name'])
     keyboard = [
         [
-            InlineKeyboardButton("Дефолтный стикерпак", callback_data=str(DEFAULT_STICKERS)),
-            InlineKeyboardButton("Кастомный стикерпак", callback_data=str(CUSTOM_STICKERS)),
+            InlineKeyboardButton("Подписаться", url="https://t.me/rasti_s_it"),
+            InlineKeyboardButton("Я уже подписан", callback_data=str(CallbackEnum.CHECK_SUBSCRIPTION.value)),
         ]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    # Instead of sending a new message, edit the message that
-    # originated the CallbackQuery. This gives the feeling of an
-    # interactive menu.
+    await query.edit_message_text(
+        text='Чтобы начать генерацию стикер пака с Вами в главной роли,\
+                 подпишитесь на Telegram-канал "Чат | Расти с IT"',
+        reply_markup=reply_markup
+    )
+    return StagesEnum.CHECK_SUBSCRIPTION.value
+
+
+async def check_subscription(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Проверить подписку на тг канал"""
+    query = update.callback_query
+    # проверка
+    logger.info("Проверка прошла успешно")
+    keyboard = [
+        [
+            InlineKeyboardButton("Дефолтный стикер пак", callback_data=str(CallbackEnum.DEFAULT_STICKERS.value)),
+            InlineKeyboardButton("Кастомный стикер пак", callback_data=str(CallbackEnum.CUSTOM_STICKERS.value)),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
     await query.edit_message_text(text="Выберите, что хотите сгенерировать", reply_markup=reply_markup)
-    return START_ROUTES
+    return StagesEnum.CHOOSE_STICKER_TYPE.value
 
 
 async def default_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
+    """Генерация обычного стикер пака"""
     query = update.callback_query
-    await query.answer()
+    user = query.from_user
+    logger.info("User %s chose default sticker pack.", user['first_name'])
 
     await query.edit_message_text(
         text="Чтобы получить ваш уникальный стикерпак, загрузите, пожалуйста, свою фотографию.\n"
@@ -82,11 +119,11 @@ async def default_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE) -
              "— если вы носите очки, то попробуйте использовать фото без них;\n"
              "— не отправляйте фото животных, бот распознаёт только лица людей."
     )
-    return AFTER_SEND_PHOTO
+    return StagesEnum.AFTER_SEND_PHOTO.value
 
 
 async def custom_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
+    """Сгенерировать шаблонный стикер пак"""
     query = update.callback_query
     await query.answer()
 
@@ -98,68 +135,53 @@ async def custom_stickers(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
              "— желательно без головного убора и очков;\n"
              "— не отправляйте фото животных, бот распознаёт только лица людей."
     )
-    return AFTER_SEND_PHOTO
+    return StagesEnum.AFTER_SEND_PHOTO.value
 
 
-async def after_send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    keyboard = [
-        [
-            InlineKeyboardButton("Подписаться", url="https://t.me/rasti_s_it"),
-            InlineKeyboardButton("Я уже подписан", callback_data=str(CHECKED_SUBSCRIBE)),
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text(
-        text='Чтобы начать генерацию стикерпака с Вами в главной роли, подпишитесь на Telegram-канал "Чат | Расти с IT"',
-        reply_markup=reply_markup
-    )
-
-    return CHECKED_SUBSCRIBE
-
-async def checked_subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-
-    query = update.callback_query
-    await query.answer()
-    await query.edit_message_text(
-        text="Происходит магия превращения, скоро все получится...\n\n"
-        "Как только все будет готов, мы пришлем уведомление",
-    )
+async def after_send_photo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Обработка фото"""
+    logger.info('after_send_photo дошло')
     return ConversationHandler.END
 
-def main() -> None:
-    """Run the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token("6767775067:AAGH2PAbT5m7r7tcDebbxdWLssk1qRCvRPs").build()
 
-    # Setup conversation handler with the states FIRST and SECOND
-    # Use the pattern parameter to pass CallbackQueries with specific
-    # data pattern to the corresponding handlers.
-    # ^ means "start of line/string"
-    # $ means "end of line/string"
-    # So ^ABC$ will only allow 'ABC'
+async def end_work(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Завершить работу"""
+    logger.info('end_work дошло')
+    return ConversationHandler.END
+
+
+def main() -> None:
+    """Функция инициализации бота"""
+    application = Application.builder().token(settings.TOKEN).build()
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            START_ROUTES: [
-                CallbackQueryHandler(default_stickers, pattern="^" + str(DEFAULT_STICKERS) + "$"),
-                CallbackQueryHandler(custom_stickers, pattern="^" + str(CUSTOM_STICKERS) + "$"),
+            StagesEnum.START_CONVERSATION.value: [
+                CallbackQueryHandler(start_check_subscription,
+                                     pattern="^" + str(CallbackEnum.START_CHECK_SUBSCRIPTION.value) + "$"),
+                CallbackQueryHandler(end_work,
+                                     pattern="^" + str(CallbackEnum.END.value) + "$"),
             ],
-            CHECKED_SUBSCRIBE: [
-                CallbackQueryHandler(checked_subscribe, pattern="^" + str(CHECKED_SUBSCRIBE) + "$"),
+            StagesEnum.CHECK_SUBSCRIPTION.value: [
+                CallbackQueryHandler(check_subscription,
+                                     pattern="^" + str(CallbackEnum.CHECK_SUBSCRIPTION.value) + "$"),
             ],
-            AFTER_SEND_PHOTO: [
-                MessageHandler(callback=after_send_photo, filters=ALL),
-            ]
+            StagesEnum.CHOOSE_STICKER_TYPE.value: [
+                CallbackQueryHandler(default_stickers, pattern="^" + str(CallbackEnum.DEFAULT_STICKERS.value) + "$"),
+                CallbackQueryHandler(custom_stickers, pattern="^" + str(CallbackEnum.CUSTOM_STICKERS.value) + "$"),
+            ],
+            StagesEnum.AFTER_SEND_PHOTO.value: [
+                CallbackQueryHandler(after_send_photo, pattern="^" + str(CallbackEnum.SEND_PHOTO.value) + "$"),
+                # CallbackQueryHandler(custom_stickers, pattern="^" + str(CallbackEnum.CUSTOM_STICKERS.value) + "$"),
+            ],
         },
         fallbacks=[CommandHandler("start", start)],
     )
-
-    # Add ConversationHandler to application that will be used for handling updates
+    # Добавляю обработчик диалога
     application.add_handler(conv_handler)
-
-    # Run the bot until the user presses Ctrl-C
+    # Запуск бота
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
